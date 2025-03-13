49ed5ac08ae3e0754a965bbfe60cbc2a33281e0e
fisayo-dev <olufisayobadina@gmail.com>
2025-03-13 21:24:43 +0100

    fox: change import style of router

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,7 +1,7 @@
-import express from "express";
+import { Router } from "express";
 import { signupUser, signinUser, signoutUser } from "../controllers/auth.controller.js";
 
-const router = express.Router()
+const router = Router()
 
 router.post('/sign-up', signupUser)
 router.post('/sign-in', signinUser)
