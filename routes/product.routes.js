490d595c3af3ccf60e8890278d135901cd62f3d8
fisayo-dev <olufisayobadina@gmail.com>
2025-03-14 00:58:46 +0100

    fix: move create, update and delete method of product to admin route

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,11 +1,9 @@
 import { Router } from "express";
-import { createProduct, deleteProduct, updateProduct, getAllProducts, getParticularProduct } from '../controllers/product.controller.js'
+import { getAllProducts, getParticularProduct } from '../controllers/product.controller.js'
 const router = Router()
 
 router.get('/', getAllProducts)
 router.get('/:id', getParticularProduct)
-router.post('/admin/:admin_id', createProduct)
-router.put('/:id', updateProduct)
-router.delete("/:id/admin/:admin_id", deleteProduct)
+
 
 export default router
