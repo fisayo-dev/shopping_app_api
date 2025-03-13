214285cf97326692c0be55e3d651c8c3842a42b1
fisayo-dev <olufisayobadina@gmail.com>
2025-03-13 21:43:23 +0100

    chore: create product routes

---
+++ controllers/product.controller.js
@@ -0,0 +1,22 @@
+const getAllProducts = async () => {
+
+}
+
+const getParticularProduct = async () => {
+
+}
+
+const createProduct = async () => {
+
+}
+
+const updateProduct = async () => {
+
+}
+
+const deleteProduct = async () => {
+
+}
+
+export { createProduct, deleteProduct, updateProduct, getAllProducts, getParticularProduct }
+
--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-
+import { createProduct, deleteProduct, updateProduct, getAllProducts, getParticularProduct } from '../controllers/product.controller.js'
 const router = Router()
 
 router.get('', getAllProducts)
