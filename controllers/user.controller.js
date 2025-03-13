85866defe23191890f84d83b79fd12fad687611f
fisayo-dev <olufisayobadina@gmail.com>
2025-03-13 21:17:41 +0100

    chore: create user controllers

---
+++ controllers/user.controller.js
@@ -0,0 +1,18 @@
+const getAllUsers = async (req, res) => {
+    
+}
+
+const getParticularUser = async (req, res) => {
+
+}
+const createUser = async (req, res) => {
+
+}
+const updateUser = async (req, res) => {
+
+}
+const deleteUser = async (req, res) => {
+
+}
+
+export {createUser, getAllUsers, getParticularUser, updateUser, deleteUser}
