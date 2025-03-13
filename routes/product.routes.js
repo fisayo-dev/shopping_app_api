58983ec30da9408d5d215ac7ed0166bb18addb15
fisayo-dev <olufisayobadina@gmail.com>
2025-03-13 21:45:16 +0100

    fix: get all product url

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { createProduct, deleteProduct, updateProduct, getAllProducts, getParticularProduct } from '../controllers/product.controller.js'
 const router = Router()
 
-router.get('', getAllProducts)
+router.get('/', getAllProducts)
 router.get('/:id', getParticularProduct)
 router.post('/admin/:admin_id', createProduct)
 router.put('/:id/admin/:admin_id', updateProduct)
