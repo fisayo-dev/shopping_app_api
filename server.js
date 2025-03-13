a25853795201ed9091515e2f0bc282b6d3eafe50
fisayo-dev <olufisayobadina@gmail.com>
2025-03-13 21:38:00 +0100

    chore: use admin and cart routes

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import mongoose from 'mongoose'
 import config from './config/index.js'
 import authRouter from './routes/auth.routes.js'
 import userRoutes from './routes/user.routes.js'
+import cartRoutes from './routes/cart.routes.js'
+import adminRoutes from './routes/admin.routes.js'
 
 const app = express()
 
@@ -11,6 +13,8 @@ app.use(express.json())
 
 app.use('/api/v1/auth/', authRouter)
 app.use('/api/v1/users/', userRoutes)
+app.use('/api/v1/carts/', cartRoutes)
+app.use('/api/v1/admin/', adminRoutes)
 
 // Listen to port
 app.listen(config.env.port, () => {
