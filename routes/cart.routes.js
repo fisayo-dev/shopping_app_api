812d61b1725732b9314fe8ec6fefa3b97c5b5383
fisayo-dev <olufisayobadina@gmail.com>
2025-03-14 21:08:23 +0100

    fix: rename route, place middleware to make it authroized

--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import { deleteItemFromCart, updateItemInCart, addItemToCart, getAllItemsInCart,  } from '../controllers/cart.controller.js'
-
+import authorize from "../middlewares/auth.middleware.js";
 
 const router = Router()
 
-router.get('/users/:user_id', getAllItemsInCart)
-router.post('/', addItemToCart)
-router.put('/:id/', updateItemInCart)
-router.delete('/:id/', deleteItemFromCart)
+router.get('/users/:user_id', authorize, getAllItemsInCart)
+router.post('/', authorize, addItemToCart)
+router.put('/:id/', authorize, updateItemInCart)
+router.delete('/:id/users/:user_id', authorize, deleteItemFromCart)
 
 export default router
