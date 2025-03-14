2b52b10377b1875262c4e0657a6e9666ca3fa762
fisayo-dev <olufisayobadina@gmail.com>
2025-03-14 01:24:11 +0100

    fix: redesigned user model

--- models/user.model.js
+++ models/user.model.js
@@ -1,16 +1,39 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    fullName: { type: String, required: true },
-    email: { type: String, required: true },
-    password: { type: String, required: true },
-    gender: { type: String, required: true },
-    telephone: { type: Number, required: true },
-    dateOfBirth: { type: Date, required: true },
-    country: { type: String, required: true },
-    lastActive: { type: Date, default: Date.now },
-    createdAt: { type: Date, default: Date.now },
-})
+    fullName: {
+        type: String,
+        required: [true, 'Full Name is required']
+    },
+    email: {
+        type: String,
+        required: [true, 'Email address is required'],
+        trim: true,
+        unique: true,
+        lowercase: true,
+        match: [/\S+@\S+\.\S+/, 'Pls fill a valid email address']
+     },
+    password: {
+        type: String,
+        required: [true, 'Password is required'],
+        minLength: 6,
+    },
+    gender: {
+        type: String,
+        required: [true, 'Gender is required'],
+        trim: true,
+    },
+    dateOfBirth: {
+        type: Date,
+        required: [true, 'Date of Birth is required'],
+    },
+    country: {
+        type: String,
+        required: true,
+        lowercase: true,
+    },
+}, { timestamps: true })
 
-const userModel = mongoose.model('User', userSchema)
-export default userModel
+
+const User = mongoose.model('User', userSchema)
+export default User
