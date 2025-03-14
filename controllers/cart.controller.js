53093fcdd2143e731458bc5c0fcb01302eac2152
fisayo-dev <olufisayobadina@gmail.com>
2025-03-14 23:13:46 +0100

    feat: get all user items in cart

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -1,5 +1,33 @@
-const getAllItemsInCart = async (req, res) => {
+import Cart from '../models/cart.model.js'
+import Product from '../models/product.model.js'
 
+const getAllItemsInCart = async (req, res, next) => {
+    const { userId } = req.params;
+
+    if (req.user._id != userId) {
+        const error = new Error("Soory, but you can't access somelse's cart")
+        error.statusCode = 401
+        throw error
+    }
+
+    // Fetch items in cart
+    const cartItems = await Cart.find({ owner: userId })
+
+    // Fetch productItems from cart
+    let productItems = []
+    cartItems.map(async (item) => {
+        const product = await Product.findById(item.productId)
+        productItems.push(product)
+    })
+
+    res.status(200).json({
+        success: true,
+        message: 'Fetched all items in cart',
+        data: {
+            ...cartItems,
+            productInCart: productItems
+        }
+    })
 }
 
 const addItemToCart = async (req, res) => {
