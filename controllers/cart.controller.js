fbcc5060556f061d7716af93e0bacbd45c75de03
fisayo-dev <olufisayobadina@gmail.com>
2025-03-14 23:20:24 +0100

    feat: add item to user cart

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -2,36 +2,56 @@ import Cart from '../models/cart.model.js'
 import Product from '../models/product.model.js'
 
 const getAllItemsInCart = async (req, res, next) => {
-    const { userId } = req.params;
+    try {
 
-    if (req.user._id != userId) {
-        const error = new Error("Soory, but you can't access somelse's cart")
-        error.statusCode = 401
-        throw error
-    }
-
-    // Fetch items in cart
-    const cartItems = await Cart.find({ owner: userId })
-
-    // Fetch productItems from cart
-    let productItems = []
-    cartItems.map(async (item) => {
-        const product = await Product.findById(item.productId)
-        productItems.push(product)
-    })
-
-    res.status(200).json({
-        success: true,
-        message: 'Fetched all items in cart',
-        data: {
-            ...cartItems,
-            productInCart: productItems
+        const { userId } = req.params;
+        
+        if (req.user._id != userId) {
+            const error = new Error("Sorry, but you can't access someone else's cart")
+            error.statusCode = 401
+            throw error
         }
-    })
+        
+        // Fetch items in cart
+        const cartItems = await Cart.find({ owner: userId })
+        
+        // Fetch productItems from cart
+        let productItems = []
+        cartItems.map(async (item) => {
+            const product = await Product.findById(item.productId)
+            productItems.push(product)
+         })
+    
+        res.status(200).json({
+            success: true,
+            message: 'Fetched all items in cart',
+            data: {
+                ...cartItems,
+                productInCart: productItems
+            }
+        })
+    } catch (error) {
+        next(error)
+    }
 }
 
-const addItemToCart = async (req, res) => {
+const addItemToCart = async (req, res, next) => {
+    try {
+        if (req.user._id != req.body.owner) {
+            const error = new Error("Sorry, but you can't add to someone else' cart")
+            error.statusCode = 401
+            throw error
+        }
+        const newCartItem = await Cart.create([{ ...req.body }])
 
+        res.status(201).json({
+            success: true,
+            message: 'New item just added to cart',
+            cart: newCartItem
+        })
+    } catch (error) {
+        next(error)
+    }
 }
 
 const updateItemInCart = async (req, res) => {
