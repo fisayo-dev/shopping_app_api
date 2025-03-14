832da1cb1987cb853850f618444981186bae6fd1
fisayo-dev <olufisayobadina@gmail.com>
2025-03-14 01:40:29 +0100

    fix: placed cretae, uodate and delete route for product in product routes file because it can been validated by admin middleware

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,13 +1,9 @@
 import { Router } from "express";
 import { signinAdmin, signoutAdmin, signupAdmin } from "../controllers/admin.controller.js";
-import { createProduct, deleteProduct, updateProduct,} from '../controllers/product.controller.js'
 const router = Router()
 
 router.post("/sign-up", signupAdmin)
 router.post("/sign-in", signinAdmin)
 router.post("/sign-out", signoutAdmin)
-router.post("/products/create", createProduct)
-router.put("/products/:id/update", updateProduct)
-router.delete("/products/:id/delete", deleteProduct)
 
 export default router;
--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { getAllProducts, getParticularProduct } from '../controllers/product.controller.js'
+import { getAllProducts, getParticularProduct, createProduct, deleteProduct, updateProduct } from '../controllers/product.controller.js'
 const router = Router()
 
 router.get('/', getAllProducts)
 router.get('/:id', getParticularProduct)
+router.post("/", createProduct)
+router.put("/:id/", updateProduct)
+router.delete("/:id/", deleteProduct)
+
 
 
 export default router
