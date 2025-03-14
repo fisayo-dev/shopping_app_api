3d577d1b970848782b1a799810848fabf7271247
fisayo-dev <olufisayobadina@gmail.com>
2025-03-14 02:31:58 +0100

    chore: used productRoutes

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import config from './config/index.js'
 import authRoutes from './routes/auth.routes.js'
 import userRoutes from './routes/user.routes.js'
 import cartRoutes from './routes/cart.routes.js'
+import productRoutes from './routes/product.routes.js'
 import adminRoutes from './routes/admin.routes.js'
 import connectToDatabase from './database/mongodb.js'
 import errMiddleware from './middlewares/error.middleware.js'
@@ -15,10 +16,12 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 
+// API Routes (endpoints)
 app.use('/api/v1/auth/', authRoutes)
 app.use('/api/v1/users/', userRoutes)
 app.use('/api/v1/carts/', cartRoutes)
 app.use('/api/v1/admin/', adminRoutes)
+app.use('/api/v1/products/', productRoutes)
 
 // Err Middleware 
 app.use(errMiddleware)
