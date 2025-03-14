5c39a77691cb45729959ca876d0ada1f5a6f87c8
fisayo-dev <olufisayobadina@gmail.com>
2025-03-14 02:24:41 +0100

    chore: configured hwt env variables

--- config/index.js
+++ config/index.js
@@ -4,7 +4,11 @@ dotenv.config()
 const config = {
     env: {
         port: process.env.PORT,
-        mongodbUrl: process.env.MONGODB_URL
+        mongodbUrl: process.env.MONGODB_URL,
+        jwt: {
+            secret: process.env.JWT_SECRET,
+            expiresIn: process.env.JWT_EXPIRES_IN
+        }
     }
 }
 
