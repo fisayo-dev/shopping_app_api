14342f20f08b4576c85098f4c701ffc266b9b001
fisayo-dev <olufisayobadina@gmail.com>
2025-03-14 16:43:09 +0100

    feat: create signup controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,28 +1,77 @@
+import mongoose from "mongoose"
 import User from "../models/user.model.js"
 import bcrypt from "bcryptjs"
+import jwt from 'jsonwebtoken'
+import config from "../config/index.js"
 
-export const signupUser = async (req, res) => {
-    const { email, fullName, password, gender, telephone, dateOfBirth, country } = req.body
-    
-    if (!email || !fullName || !password || !gender || !telephone || !dateOfBirth || !country) return res.status(400).json({ message: 'Ensure all field are provided with data' })
-    
-    const hashedPassword = bcrypt.hashSync(password, 10)
+export const signupUser = async (req, res, next) => { 
+    // A mongoose transaction session
+    const session = await mongoose.startSession()
+    session.startTransaction()
 
     try {
+        // Create a  new user
+        const { name, email, password, gender, dateOfBirth, country } = req.body
         
-        const newUser = new User({
-            email, fullName, hashedPassword, gender, telephone, dateOfBirth, country
+        // Check if suer exist
+        const existingUser = await User.findOne({ email })
+        if (existingUser) {
+            const error = new Error('User already exists')
+            error.statusCode = 409;
+            throw error
+        }
+
+        // Hash password
+        const salt = bcrypt.genSalt(10)
+        const hashedPassword = bcrypt.hash(password, salt)
+
+        // Create new user into database
+        const newUser = await User.create({
+            name, email, password: hashedPassword, gender, dateOfBirth, country
+        }, { session })
+        
+        // Generate token for user
+        const token = jwt.sign({ userId: newUser._id }, config.env.jwt.secret, { expiresIn: config.env.jwt.expiresIn })
+
+
+        await session.commitTransaction()
+        session.endSession()
+        res.status(201).json({
+            success: true,
+            message: 'User created succesfully',
+            data: {
+                token,
+                user: newUser,
+            }
         })
+
         
-        const createdUser = await newUser.save()
-        
-        res.status(201).send({
-            message: 'Your account has been successfully created',
-            userId: createdUser._id
-        })
     } catch (err) {
-        res.status(500).json({ message: 'Error occurred when trying to create user', error: err.message })
+        await session.abortTransaction();
+        session.endSession();
+        next(error)
     }
+    // const { email, fullName, password, gender, telephone, dateOfBirth, country } = req.body
+    
+    // if (!email || !fullName || !password || !gender || !telephone || !dateOfBirth || !country) return res.status(400).json({ message: 'Ensure all field are provided with data' })
+    
+    // const hashedPassword = bcrypt.hashSync(password, 10)
+
+    // try {
+        
+    //     const newUser = new User({
+    //         email, fullName, hashedPassword, gender, telephone, dateOfBirth, country
+    //     })
+        
+    //     const createdUser = await newUser.save()
+        
+    //     res.status(201).send({
+    //         message: 'Your account has been successfully created',
+    //         userId: createdUser._id
+    //     })
+    // } catch (err) {
+    //     res.status(500).json({ message: 'Error occurred when trying to create user', error: err.message })
+    // }
 }
 
 export const signinUser = async (req, res) => {
