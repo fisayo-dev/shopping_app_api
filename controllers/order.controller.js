729189f9cf43ce9d548af67f0654ff603b1e2e55
fisayo-dev <olufisayobadina@gmail.com>
2025-03-21 00:05:26 +0100

    chore: create controllers, add get spefic order route

---
+++ controllers/order.controller.js
@@ -0,0 +1,11 @@
+export const makeOrders = async (req, res, next) => {
+
+}
+
+export const getOrders = async (req, res, next) => {
+    
+}
+
+export const getOrder = async (req, res, next) => {
+    
+}
--- routes/order.routes.js
+++ routes/order.routes.js
@@ -1,8 +1,10 @@
 import { Router } from 'express'
+import { makeOrders, getOrder, getOrders } from '../controllers/order.controller'
 
 const router = Router()
 
-router.get('/my-orders', getOrder)
+router.get('/my-orders', getOrders)
+router.get('/my-orders/:id', getOrder)
 router.post('/make', makeOrders)
 
 export default router
