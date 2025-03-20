0aa4c7316009838e9953c2887304f44598b5aad8
fisayo-dev <olufisayobadina@gmail.com>
2025-03-20 21:54:34 +0100

    fix: protect GET all user route - restrict access to admin only

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { createUser, getAllUsers, getParticularUser, updateUser, deleteUser } from '../controllers/user.controller.js'
 import authorize from '../middlewares/auth.middleware.js'
+import adminMiddleware from '../middlewares/admin.middleware.js'
 
 
 const router = Router()
 
-router.get('/', getAllUsers)
+router.get('/', adminMiddleware, getAllUsers)
 router.get('/:id', authorize, getParticularUser)
 router.post('/', createUser)
 router.put('/:id', updateUser)
