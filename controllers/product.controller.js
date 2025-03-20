5bca617c3e79d4c6b9606abc53bfa5f27ccd9366
fisayo-dev <olufisayobadina@gmail.com>
2025-03-20 21:51:47 +0100

    chore: sort products chronologically

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -8,7 +8,7 @@ const getAllProducts = async (req, res, next) => {
             message: 'Got all products',
             data: {
                 length: products.length > 0 ? products.length : 'The product database is empty!',
-                products
+                products: products.sort((a,b) => b.createdAt - a.createdAt)
             }
         })
 
