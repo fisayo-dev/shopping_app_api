438ae5d44fc6bb8d19cef7ef6734ef3a678ec349
fisayo-dev <olufisayobadina@gmail.com>
2025-03-21 00:38:36 +0100

    chore: create order controller, secure order route with authorize middleware

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -1,4 +1,46 @@
+import Cart from '../models/cart.model.js';
+import Product from '../models/product.model.js';
+import Order from '../models/order.model.js';
+
 export const makeOrders = async (req, res, next) => {
+    // Check if user has anything in cart to order
+    const cartItemsToOrdered = await Cart.find({ owner: req.user._id })
+    if (!cartItemsToOrdered) { 
+        const error = new Error("Sorry but there is nothing in your cart to order")
+        error.statusCode = 404
+        next(error)
+    }
+    
+    // Get all the id(s) of products ordered by the user
+    const productsIds = cartItemsToOrdered.map((item) => item.productId)
+
+    // Get order total price
+    const productItems = await Promise.all(
+        productsIds.map(async (id) => {
+            const product = await Product.findById(id);
+            const productInCart = await Cart.find({ owner: req.user._id, productId: id })
+            return product ? {...product.toObject(), amount: productInCart.amount} : null;
+        })
+    );
+
+    const totalPrice = productItems.reduce((a, b) => a.amount + b.amount)
+    
+    
+    // Populate order schema
+    await Order.create({
+        productsOrdered: productsIds,
+        totalPrice
+    })
+
+    res.status(201).json({
+        success: true,
+        message: 'Your order was successfull, Chill. Your product will soon be delivered to you! ',
+        data: {
+            productsOrdered: productsIds.length, 
+            orderPrice: totalPrice 
+        }
+    })
+
 
 }
 
--- routes/order.routes.js
+++ routes/order.routes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express'
 import { makeOrders, getOrder, getOrders } from '../controllers/order.controller'
+import authorize from '../middlewares/auth.middleware'
 
 const router = Router()
 
-router.get('/my-orders', getOrders)
-router.get('/my-orders/:id', getOrder)
-router.post('/make', makeOrders)
+router.get('/my-orders', authorize, getOrders)
+router.get('/my-orders/:id', authorize, getOrder)
+router.post('/make', authorize, makeOrders)
 
 export default router
