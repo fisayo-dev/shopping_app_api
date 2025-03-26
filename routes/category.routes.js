18a71816438ad0dd7e73a70168848eff822cd627
fisayo-dev <olufisayobadina@gmail.com>
2025-03-26 12:41:00 +0100

    fix: import cart controllers

--- routes/category.routes.js
+++ routes/category.routes.js
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 import adminMiddleware from '../middlewares/admin.middleware.js'
+import { getAllCategories,createCategory, updateCategory, deleteCateogry } from '../controllers/category.controller.js'
 
 const router = Router()
 
