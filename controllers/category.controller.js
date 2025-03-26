294a4cf963438b94f0f339a43c1b62986a50b025
fisayo-dev <olufisayobadina@gmail.com>
2025-03-26 12:18:41 +0100

    chore: initiate empty controller functions

---
+++ controllers/category.controller.js
@@ -0,0 +1,12 @@
+export const getAllCategories = async (req,res,next) => {
+    
+}
+export const createCategory = async (req,res,next) => {
+
+}
+export const updateCategory = async (req,res,next) => {
+
+}
+export const deleteCateogry = async (req,res,next) => {
+
+}
