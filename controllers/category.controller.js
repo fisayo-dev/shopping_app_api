d21116160e4494776939927d6663ecb8b2659b68
fisayo-dev <olufisayobadina@gmail.com>
2025-03-26 12:19:14 +0100

    feat: make GET route controller for category

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -1,5 +1,17 @@
+import Category from "../models/category.model.js"
+
 export const getAllCategories = async (req,res,next) => {
-    
+    try {
+        const categories = await Category.find()
+        res.status(200).json({
+            success: true,
+            message: 'Found all categories',
+            categories,
+            length: categories.length
+        })
+    } catch (error) {
+        next(error)
+    }
 }
 export const createCategory = async (req,res,next) => {
 
